71fbdbc62f9934e375997750ac581023b78ad7b4
Aggebitter <david.agner@gmail.com>
2016-12-20 18:43:55 +0100

    Add files via upload

---
+++ Example/ESP_LoRa_OLED/ESP_LoRa_OLED.ino
@@ -0,0 +1,68 @@
+
+
+
+
+#include <SPI.h>
+#include <sx127xRegs-LoRa.h>
+#include <SX127x.h>
+#include "config.h"
+
+#define OLED_DC 4 //D2
+#define OLED_CS 2 //D4
+#include <SSD1306Ascii.h>
+#include <SSD1306AsciiSpi.h>
+
+
+SX127x SX127x(serialDebug); // in config.h Serial debug 1= debug, 0= No debug
+SSD1306AsciiSpi oled;
+
+long int payload = 1;
+int adressModule = 129;
+int payloadModule = 77;
+int message;
+const int W = 1; // Write register
+const int R = 0; // Read register
+
+void setup()
+{
+  oled.begin(&Adafruit128x64, OLED_CS, OLED_DC);
+  oled.setFont(Adafruit5x7);  
+  SX127x.init(15); // first module
+ // SX127x.init(16); // second module
+ // SX127x.init(2); // third module
+
+}
+
+void loop()
+{ 
+for (long int i=0; i <= 1024; i++){
+
+message = SX127x.single(1, adressModule, payloadModule, W);
+ Serial.println(message, HEX);
+  //oled.clear(); 
+  oled.setCursor(1,1); 
+  oled.print("AGGE's mojj");
+  oled.setCursor(1,2);
+  oled.print("    ");
+  oled.setCursor(1,2);
+  oled.print(payloadModule);
+  oled.setCursor(1,3);
+  oled.print("    ");
+  oled.setCursor(1,3);
+  oled.println(message);
+  oled.println();
+  oled.print("messages:");
+  
+  oled.setCursor(64,6);
+  oled.set2X();
+  oled.print("      ");
+  oled.setCursor(64,6);
+  oled.println(i);
+  oled.set1X();
+ wdt_disable();
+}
+
+
+
+
+}
---
+++ Example/ESP_LoRa_OLED/config.h
@@ -0,0 +1,40 @@
+ /* WeMos D1 R2 mini pinout
+ *  Pin     Function                    ESP-8266 pin    Arduino Pin RMF9x
+ *  TX      TXD                           TXD           1
+ *  RX      RXD                           RXD           3
+ *  D0      IO                            GPIO16        16
+ *  D1      IO/SCL                        GPIO5         5
+ *  D2      IO/SDA                        GPIO4         4
+ *  D3      IO, 10K Pull Up               GPIO0         0
+ *  D4      IO, 10K Pull Up BUILTIN_LED   GPIO12        2
+ *  D5      IO, SCK                       GPIO14        14
+ *  D6      IO, MISO                      GPIO12        12
+ *  D7      IO, MOSI                      GPIO13        13
+ *  D8      IO, 10K Pull Down             GPIO15        15
+ *  A0      Analog input, Max 3.3V input  A0            A0
+ *  G       Ground                        GND
+ *  5V      5V
+ *  3V3     3.3V                          3.3V
+ *  RST     Reset                         RST
+*/
+
+#define RESET_WAIT_MS 7
+
+// The crystal oscillator frequency of the module
+#define RH_RF95_FXOSC 32000000.0
+
+// The Frequency Synthesizer step = RH_RF95_FXOSC / 2^^19
+#define RH_RF95_FSTEP  (RH_RF95_FXOSC / 524288)
+
+#define LORA_BASE_LF   (RFLR_OPMODE_LONGRANGEMODE_ON|RFLR_OPMODE_ACCESSSHAREDREG_DISABLE|RFLR_OPMODE_FREQMODE_ACCESS_LF)
+const char modemMode = 'L';
+//const int sx127xSelectPin_A = 15;
+//const int DIO0_A = 2; // TX RX complete
+//const int DIO5_A = 0; // Mode change complete
+// const boolean dualLora =1; // skipa och ta i driver init
+//const int sx127xSelectPin_B = 16;
+//const int DIO0_B = 4; // TX RX complete
+//const int DIO5_B = 5; // Mode change complete
+const boolean useDisplay = false ;
+const int serialDebug = 1 ;
+
